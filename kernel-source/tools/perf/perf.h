5ebddd73e735ab15760bbdc141e4d3beeeeda5ca
Allen <allen.chang@deepmentor.ai>
2025-09-12 10:22:29 +0800

    add linux kernel

---
+++ kernel-source/tools/perf/perf.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _PERF_PERF_H
+#include <stdbool.h>
+
+#define _PERF_PERF_H
+
+#ifndef MAX_NR_CPUS
+#define MAX_NR_CPUS			2048
+#endif
+
+enum perf_affinity {
+	PERF_AFFINITY_SYS = 0,
+	PERF_AFFINITY_NODE,
+	PERF_AFFINITY_CPU,
+	PERF_AFFINITY_MAX
+};
+
+#endif
