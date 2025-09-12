28bc9be1e2898e757073490b2238694836d503e0
Allen <allen.chang@deepmentor.ai>
2025-09-12 09:30:04 +0800

    add optee-so

---
+++ optee-os/4.4.0/git/core/drivers/crypto/hisilicon/sub.mk
@@ -0,0 +1,10 @@
+srcs-y += hisi_qm.c
+srcs-y += sec_main.c
+srcs-y += sec_hash.c
+srcs-y += sec_hmac.c
+srcs-y += sec_cipher.c
+srcs-y += sec_authenc.c
+srcs-$(CFG_HISILICON_ACC_V3) += hpre_main.c
+srcs-$(CFG_HISILICON_ACC_V3) += hpre_dh.c
+srcs-$(CFG_HISILICON_ACC_V3) += hpre_ecc.c
+srcs-$(CFG_HISILICON_ACC_V3) += hpre_montgomery.c
